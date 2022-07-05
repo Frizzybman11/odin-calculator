719cada2e529c788496cac1779756ecb4054b4ad
Frizzybman11 <Frizzybman11@aol.com>
2022-07-05 11:08:55 -0400

    Create operate function
    Takes two numbers and calls the appropriate function to calculate them

--- calculator.js
+++ calculator.js
@@ -1,6 +1,26 @@
 let num1 = 0;
 let num2 = 0;
+let operator = "";
 let total = 0;
+
+function operate(a, b, c) {
+    operator = c;
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+    return total;
+}
 
 function add(a, b) {
     num1 = a;
