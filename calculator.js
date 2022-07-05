8ac798fab592f3f3e6dc613a012197dbaf692512
Frizzybman11 <Frizzybman11@aol.com>
2022-07-05 14:54:52 -0400

    Create + complete calculator event listener
    Calculator should function properly, allowing the user
    to add, subtract, multiply, and divide numbers. A number up to 16 characters
    in length may be used.

--- calculator.js
+++ calculator.js
@@ -1,51 +1,83 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
 let total = 0;
+let display = document.querySelector("#display");
+let miniDisplay = document.querySelector("#miniDisplay");
 
-function operate(a, b, c) {
-    operator = c;
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(num1 != "" && display.textContent != "" && button.className === "opButton"){
+            num2 = Number(display.textContent)
+            operate(num1, num2, operator);
+            display.textContent = total;
+            total = 0;
+            operator = button.textContent;
+            miniDisplay.textContent = display.textContent + " " + operator + " ";
+            display.textContent = "";
+        } else if(display.textContent === "" && miniDisplay.textContent != "" && button.className === "opButton"){
+            operator = button.textContent;
+            num1 = Number(miniDisplay.textContent);
+            miniDisplay.textContent = num1 + " " + operator + " "; 
+        } else if(display.textContent != "" && button.className === "opButton"){
+            operator = button.textContent;
+            num1 = Number(display.textContent);
+            console.log("num1: " + num1);
+            miniDisplay.textContent = display.textContent + " " + operator + " ";
+            display.textContent = "";
+        } else if(num1 != "" && display.textContent != "" && button.id === "equalsButton"){
+            num2 = Number(display.textContent)
+            console.log("num2: " + num2);
+            operate(num1, num2, operator);
+            console.log("total: " + total);
+            miniDisplay.textContent = total;
+            display.textContent = "";
+        } else if(display.textContent.length >= 16){
+
+        } else if(display === ""){
+            display.textContent = button.textContent;
+        } else if(display != "") {
+            display.textContent = display.textContent + button.textContent;
+        }
+    });
+});
+
+function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
-            add(a, b);
+            add(num1, num2);
             break;
         case "-":
-            subtract(a, b);
+            subtract(num1, num2);
             break;
         case "*":
-            multiply(a, b);
+            multiply(num1, num2);
             break;
         case "/":
-            divide(a, b);
+            divide(num1, num2);
             break;
     }
     return total;
 }
 
-function add(a, b) {
-    num1 = a;
-    num2 = b;
+function add(num1, num2) {
     total = num1 + num2;
     return total;
 }
 
-function subtract(a, b) {
-    num1 = a;
-    num2 = b;
+function subtract(num1, num2) {
     total = num1 - num2;
     return total;
 }
 
-function multiply(a, b) {
-    num1 = a;
-    num2 = b;
+function multiply(num1, num2) {
     total = num1 * num2;
     return total;
 }
 
-function divide(a, b) {
-    num1 = a;
-    num2 = b;
+function divide(num1, num2) {
     total = num1 / num2;
     return total;
 }
