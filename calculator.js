6bcc59fe53773b6d529033fd86dea4b73dca724e
Frizzybman11 <Frizzybman11@aol.com>
2022-07-05 10:53:19 -0400

    Create basic operator functions
    Functions for add/subtract/multiply/divide

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let num1 = 0;
+let num2 = 0;
+let total = 0;
+
+function add(a, b) {
+    num1 = a;
+    num2 = b;
+    total = num1 + num2;
+    return total;
+}
+
+function subtract(a, b) {
+    num1 = a;
+    num2 = b;
+    total = num1 - num2;
+    return total;
+}
+
+function multiply(a, b) {
+    num1 = a;
+    num2 = b;
+    total = num1 * num2;
+    return total;
+}
+
+function divide(a, b) {
+    num1 = a;
+    num2 = b;
+    total = num1 / num2;
+    return total;
+}
