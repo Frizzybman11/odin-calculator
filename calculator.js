09bcebc6c66b553471eff5826e9baec3043b95e3
Frizzybman11 <Frizzybman11@aol.com>
2022-07-07 11:31:51 -0400

    Redo calculator script
    Started from scratch with script and got it working properly.
    Cleaner than before, but still needs work.

--- calculator.js
+++ calculator.js
@@ -6,75 +6,111 @@ let display = document.querySelector("#display");
 let miniDisplay = document.querySelector("#miniDisplay");
 let sign = 0;
 
-const buttons = document.querySelectorAll("button");
+const mainButtons = document.querySelectorAll(".mainButton");
+const endButtons = document.querySelectorAll(".endButton");
 
-buttons.forEach((button) => {
+mainButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        if(num1 != "" && display.textContent != "" && button.className === "opButton"){
-            num2 = Number(display.textContent)
-            operate(num1, num2, operator);
-            display.textContent = total;
-            num1 = total;
-            operator = button.textContent;
-            total = 0;
-            miniDisplay.textContent = display.textContent + " " + operator + " ";
-            display.textContent = "";
-        } else if(display.textContent != "" && miniDisplay.textContent != "" && button.className === "opButton"){
-            operator = button.textContent;
-            num1 = Number(miniDisplay.textContent);
-            num2 = Number(display.textContent);
-            operate(num1, num2, operator);
-            miniDisplay.textContent = total;
-            display.textContent = "";
-        } else if(display.textContent === "" && miniDisplay.textContent != "" && button.className === "opButton"){
-            operator = button.textContent;
-            num1 = Number(miniDisplay.textContent);
-            miniDisplay.textContent = num1 + " " + operator + " "; 
-        } else if(display.textContent != "" && button.className === "opButton"){
-            operator = button.textContent;
-            num1 = Number(display.textContent);
-            console.log("num1: " + num1);
-            miniDisplay.textContent = display.textContent + " " + operator + " ";
-            display.textContent = "";
-        } else if (num1 != "" && display.textContent === "" && button.id === "equalsButton"){
+        if(button.classList.contains("numButton")) {
+            displayNum(button);
+        } else if (display.textContent === "" && num2 === "") {
 
-        } else if(num1 != "" && display.textContent != "" && button.id === "equalsButton"){
-            num2 = Number(display.textContent)
-            console.log("num2: " + num2);
+        } else if (button.classList.contains("opButton") && display.textContent != ""){
+            operator = button.textContent;
+            inputNum1(button);
+            console.log("operator: " + operator);
+        } else if(button.classList.contains("opButton") && num1 === "") {
+            operator = button.textContent;
+            inputNum1(button);
+            console.log("operator: " + operator);
+        } else if(button.classList.contains("opButton") && num1 != "") {
+            inputNum2(button);
             operate(num1, num2, operator);
+            roundTotal(total);
             console.log("total: " + total);
-            miniDisplay.textContent = total;
+            operator = button.textContent;
+            console.log("operator: " + operator);
+            num1 = total;
+            num2 = "";
+            total = 0;
+            miniDisplay.textContent = num1 + " " + operator;
             display.textContent = "";
-        // if (a){
-        // } else if (num1 != "" && display.textContent != "" && button.className === "opButton"){
-        //     num2 = Number(display.textContent);
-        //     operate(num1, num2, operator);
-        //     operator = button.textContent;
-        //     num1 = total;
-        //     miniDisplay.textContent = total + " " + operator;
-        //     display.textContent = "";
-        // } else if (display.textContent != "" && button.className === "opButton"){
-        //     num1 = Number(display.textContent);
-        //     operator = button.textContent;
-        //     miniDisplay.textContent = display.textContent + " " + operator;
-        //     display.textContent = "";
-        } else if(display.textContent.length >= 16){
-
-        } else if(button.textContent === "±"){
-            if (display.textContent != ""){
-            sign = Number(display.textContent)
-            if(Math.sign(sign) === -1){
-                display.textContent = Math.abs(sign)
-            } else
-                display.textContent = -Math.abs(sign)
-            }
-        } else if(display === ""){
-            display.textContent = button.textContent;
-        } else if(display != "") {
-            display.textContent = display.textContent + button.textContent;
         }
-    });
-});
+    })
+})
+
+endButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.id === "clearButton"){
+            num1 = "";
+            num2 = "";
+            operator = "";
+            total = 0;
+            sign = 0;
+            display.textContent = "";
+            miniDisplay.textContent = "";
+            document.getElementById("display").style.fontSize = "";
+            document.getElementById("miniDisplay").style.fontSize = "";
+        } else if (display.textContent.length >= 16 && button.id === "equalsButton" && num1 != ""){
+            inputNum2(button);
+            operate(num1, num2, operator);
+            roundTotal(total);
+            total = total.toExponential();
+            console.log("total: " + total);
+            document.getElementById("display").style.fontSize = "20px";
+            document.getElementById("miniDisplay").style.fontSize = "20px";
+            miniDisplay.textContent = num1 + " " + operator + " " + num2 + " =";
+            display.textContent = total;
+        } else if(button.id === "equalsButton" && num1 != ""){
+            inputNum2(button);
+            operate(num1, num2, operator);
+            roundTotal(total);
+            console.log("total: " + total);
+            miniDisplay.textContent = num1 + " " + operator + " " + num2 + " =";
+            display.textContent = total;
+        }
+    })
+})
+
+function displayNum(button) {
+    if(display.textContent.length >= 16){
+
+    }
+    else if (display.textContent === "") {
+      display.textContent = button.textContent;
+  } else if (display.textContent != ""){
+    display.textContent = display.textContent + button.textContent;
+  }
+}
+
+function inputNum1(button) {
+        num1 = Number(display.textContent);
+        console.log("num1: " + num1);
+        miniDisplay.textContent = num1 + " " + operator;
+        display.textContent = "";
+}
+
+function inputNum2(button) {
+        num2 = Number(display.textContent);
+        console.log("num2: " + num2);
+        miniDisplay.textContent = total;
+        display.textContent = "";
+}
+
+function changeSign(button) {
+    if (display.textContent != ""){
+        sign = Number(display.textContent);
+        if(Math.sign(sign) === -1){
+            display.textContent = Math.abs(sign);
+        } else
+        display.textContent = -Math.abs(sign);
+    }
+}
+
+function roundTotal(num) {
+    num = Math.round((num * 100) / 100);
+    return num;
+}
 
 function operate(num1, num2, operator) {
     switch (operator) {
